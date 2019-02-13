8c3e049a7380a5e7b5bf0a6770bf872800ea360e
Ian Hierl <ihierl@ku.edu>
2019-02-13 00:28:02 -0600

    Finish bare minimum definitions of Board and Tile classes
    Completed integrating game play logic with Qt widgets.
    To do:
     - [ ] Move setMines() to activate at first click to prevent loss on first turn
     - [ ] Tie timer into gameplay, activate at first click
     - [ ] Add comments for documentation generation
     - [ ] Clean up formatting and edit for readability

--- board.py
+++ board.py
@@ -7,7 +7,7 @@ from tile import Tile
 
 
 class Board(QWidget):
-    def __init__(self, rows, cols, count = 1):
+    def __init__( self, rows, cols, count = 1 ):
         super().__init__()
 
         self.rows = rows
@@ -16,39 +16,70 @@ class Board(QWidget):
         self.mineIndices = []
         self.mineCount = count
         self.flagCount = 0
+        self.lost = False
+        self.active = True
         self.boardLayout = QGridLayout() #Loads a grid layout
 
         # create the list of rows * cols unique tiles
-        for i in range(0, rows):
-          self.tiles.append([])
-          for j in range(0, cols):
-            self.tiles[i].append(Tile())
-            self.boardLayout.addWidget(self.tiles[i][j], i, j)
+        for i in range( 0, rows ):
+            self.tiles.append( [] )
+            for j in range( 0, cols ):
+                self.tiles[i].append( Tile(i, j) )
+                self.tiles[i][j].clicked.connect( self.clickHandler )
+                self.boardLayout.addWidget( self.tiles[i][j], j, i )
         
-        # assign n tiles to be mines
-        for n in range(0, count):
-          while True:
-            i = random.randint(0, rows - 1)
-            j = random.randint(0, cols - 1)
-            if not self.tiles[i][j].isMine():
-              self.tiles[i][j].setMine()
-              self.mineIndices.append((i,j))
-              print("Setting mine at: %d, %d" % (i, j)) 
-              # increment the mine count on neighboring tiles
-              for (y, x) in self.getNeighbors(i, j):
-                self.tiles[y][x].incCount()
-              break
+        # assign tiles to be mines
+        self.setMines( count )
 
-        self.setLayout(self.boardLayout)
+        self.setLayout( self.boardLayout )
 
-    def getNeighbors(self, row, col):
-      indices = []
-      for i in [row - 1, row, row + 1]:
-        for j in [col - 1, col, col + 1]:
-          validRow = not (i < 0 or i >= self.rows)
-          validCol = not (j < 0 or j >= self.cols)
-          if validRow and validCol:
-            indices.append((i,j))
-      return indices
+    def getNeighbors( self, row, col ):
+        indices = []
+        for i in [ row - 1, row, row + 1 ]:
+            validRow = not ( i < 0 or i >= self.rows )
+            for j in [ col - 1, col, col + 1 ]:
+                validCol = not ( j < 0 or j >= self.cols )
+                if validRow and validCol:
+                    indices.append( (i, j) )
+        return indices
     
+    def setMines( self, count ):
+        for n in range( 0, count ):
+            i = random.randint( 0, self.rows - 1 )
+            j = random.randint( 0, self.cols - 1 )
+            if not self.tiles[i][j].isMine():
+                self.tiles[i][j].setMine()
+                self.mineIndices.append( (i, j) )
+                print( "Setting mine at: %d, %d" % (i, j) )
+                # increment the mine count on neighboring tiles
+                for (y, x) in self.getNeighbors( i, j ):
+                  self.tiles[y][x].incCount()
+                break
+        self.minesSet = True
+
+    # returns True if Tile successfully flips, False if Tile is already flipped
+    # returns True even if Tile is a mine
+    def flip( self, i, j ):
+        # reveal tile and set temp to return value, True if flipped False if not
+        temp = self.tiles[i][j].flip()
+       
+        if not temp:
+            return temp
+        elif self.tiles[i][j].isMine():
+            self.lost = True
+            self.active = False
+        elif self.tiles[i][j].getCount() == 0:
+            for (row, col) in self.getNeighbors( i, j ):
+                self.flip( row, col )
+        return temp
+
+    def clickHandler(self):
+        sender = self.sender()
+        (i, j) = sender.getIndices()
+        print( "Click detected at %d, %d" % (i, j) )
+        temp = self.flip( i, j )
+        if not temp:
+            print( "Unable to flip already visibile tile" )
+        if self.tiles[i][j].isMine():
+            print( "Mine uncovered" )
 
--- tile.py
+++ tile.py
@@ -4,14 +4,51 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 class Tile(QPushButton):
-    def __init__(self):
-        super().__init__("")
+    def __init__( self, i, j ):
+        super().__init__("?")
 
+        self.row = i
+        self.col = j
+        self.count = 0
+        self.mine = False
+        self.visible = False
+    
+    def getIndices( self ):
+        return ( self.row, self.col )
+
+    def getCount(self):
+        return self.count
+    
     def incCount(self):
+        self.count += 1
         return True
 
     def setMine(self):
-        return True
+        if self.mine:
+            return False
+        else:
+            self.mine = True
+            return True
     
     def isMine(self):
-        return False
+        return self.mine
+    
+    def flip(self):
+        if self.visible:
+            return False
+        else:
+            self.visible = True
+            self.update()
+            return True
+
+    def update(self):
+        if not self.visible:
+            self.setText( "?" )
+        elif self.mine:
+            self.setText( "M" )
+        else:
+            self.setText( "%d" % self.count )
+        return None        
+
+    def isFlipped(self):
+        return self.visible
