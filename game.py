874fdb518dc5b5747ce042fad3d7e4f7e44fb0f5
Cameron Kientz <noremackientz@gmail.com>
2019-02-16 00:34:07 -0600

    Separated game and menu windows into individual classes. Creates new window on button press

--- game.py
+++ game.py
@@ -1,65 +1,27 @@
 import sys
 from Styles import StyleSheet
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIntValidator
 from PyQt5.QtCore import *
 from board import Board
 
 class Game(QWidget):
-    def __init__(self):
+    def __init__(self,rows,cols,count):
         super().__init__()
 
         self.setWindowTitle('Minesweeper')  #Sets title of main QWidget
         self.setGeometry(300, 300, 250, 150)    #Sets size of window
 
         self.layout = QVBoxLayout(self) #Implements a vertical blox layout
-        self.layout.addWidget(self.getSize())
 
         self.setLayout(self.layout) #Adds layout to main widget
 
-    okPressed = False
-    rows = 2
-    cols = 2
-
-    def getSize(self):
-        inputWidget = QWidget()
-        inputLayout = QGridLayout()
-        inputWidget.setLayout(inputLayout)
-        self.grid = []
-        self.grid.append([])
-
-        inputLayout.addWidget(QLabel('# of Rows'), 0, 0)
-        self.rowInput = QLineEdit(self)
-        self.intValidator = QIntValidator(self)
-        self.rowInput.setValidator(self.intValidator)
-        inputLayout.addWidget(self.rowInput, 0 , 1)
-
-        inputLayout.addWidget(QLabel('# of Columns'), 1, 0)
-        self.colInput = QLineEdit(self)
-        self.colInput.setValidator(self.intValidator)
-        inputLayout.addWidget(self.colInput, 1, 1)
-
-        inputLayout.addWidget(QLabel('# of Mines'), 2, 0)
-        self.mineInput = QLineEdit(self)
-        self.mineInput.setValidator(self.intValidator)
-        inputLayout.addWidget(self.mineInput, 2, 1)
-
-        okButton = QPushButton('OK')
-        inputLayout.addWidget(okButton, 2, 3)
-        okButton.clicked.connect(self.gamestart)
-
-        return inputWidget
-
-    def gamestart(self):
-        if len(self.rowInput.text()) and len(self.colInput.text()) and len(self.mineInput.text()) > 0 :
-            if (int(self.rowInput.text()) and int(self.colInput.text()) > 1) and (int(self.mineInput.text()) < (int(self.rowInput.text())*int(self.colInput.text()))):
-                okPressed = True
-                self.startTimer()
-                rows = int(self.rowInput.text())
-                cols = int(self.colInput.text())
-                self.layout.addWidget(self.initTimerWidget())   #Executes the initTimerWidget and adds it to the main widget
-                self.board = Board(rows, cols)
-                self.layout.addWidget(self.board)
+        self.rows = rows
+        self.cols = cols
+        self.count = count
+        self.startTimer()
+        self.layout.addWidget(self.initTimerWidget())   #Executes the initTimerWidget and adds it to the main widget
+        self.board = Board(self.rows, self.cols, self.count)
+        self.layout.addWidget(self.board)
 
     def initTimerWidget(self):  #Initializes a timer widget which has a horizontal box layout and adds a buttona dn label
         timerWidget = QWidget()
--- main.py
+++ main.py
@@ -3,10 +3,60 @@ from Styles import StyleSheet
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from game import Game
+from PyQt5.QtGui import QIntValidator
+
+
+
+class MenuWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Minesweeper')  #Sets title of main QWidget
+        self.setGeometry(300, 300, 250, 150)    #Sets size of window
+
+        self.layout = QVBoxLayout(self) #Implements a vertical blox layout
+        self.layout.addWidget(self.getSize())
+        self.setLayout(self.layout) #Adds layout to main widget
+
+
+    def gamestart(self,):
+        if len(self.rowInput.text()) and len(self.colInput.text()) and len(self.mineInput.text()) > 0 :
+            if (int(self.rowInput.text()) and int(self.colInput.text()) > 1) and (int(self.mineInput.text()) < (int(self.rowInput.text())*int(self.colInput.text()))):
+                self.game = Game(int(self.rowInput.text()),int(self.colInput.text()),int(self.mineInput.text()))
+                self.game.show()
+
+
+    def getSize(self):
+        inputWidget = QWidget()
+        inputLayout = QGridLayout()
+        inputWidget.setLayout(inputLayout)
+
+        inputLayout.addWidget(QLabel('# of Rows'), 0, 0)
+        self.rowInput = QLineEdit(self)
+        self.intValidator = QIntValidator(self)
+        self.rowInput.setValidator(self.intValidator)
+        inputLayout.addWidget(self.rowInput, 0 , 1)
+
+        inputLayout.addWidget(QLabel('# of Columns'), 1, 0)
+        self.colInput = QLineEdit(self)
+        self.colInput.setValidator(self.intValidator)
+        inputLayout.addWidget(self.colInput, 1, 1)
+
+        inputLayout.addWidget(QLabel('# of Mines'), 2, 0)
+        self.mineInput = QLineEdit(self)
+        self.mineInput.setValidator(self.intValidator)
+        inputLayout.addWidget(self.mineInput, 2, 1)
+
+        self.okButton = QPushButton('OK')
+        inputLayout.addWidget(self.okButton, 2, 3)
+        self.okButton.clicked.connect(self.gamestart)
+
+        return inputWidget
 
 def main():
     app = QApplication(sys.argv)    #Creates the QT application
-    menu = Game() #Initializes the main widget
+    menu = MenuWindow() #Initializes the main widget
     menu.show() #Displays the main widget
-    app.exec_() #Tells the app to run the main loop
+    sys.exit(app.exec_()) #Tells the app to run the main loop
+
 main()
